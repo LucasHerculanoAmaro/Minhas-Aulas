cb4117db2d9e73ae6cb818bdfee659300d2c7121
Lucas Amaro <lucash.96@hotmail.com>
2024-05-19 22:57:56 -0300

    adicionando atualizações do projeto JornalESP

--- Euripedes Simões de Paula/jornalESP/src/app/app.component.ts
+++ Euripedes Simões de Paula/jornalESP/src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { MenuComponent } from "./menu/menu.component";
 import { RodapeComponent } from './rodape/rodape.component';
 import { NewsComponent } from "./news/news.component";
+import { CalendarioComponent } from './calendario/calendario.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuComponent, RodapeComponent, InicioComponent, NewsComponent]
+    imports: [
+      RouterOutlet, 
+      CommonModule,
+      MenuComponent, 
+      RodapeComponent, 
+      InicioComponent, 
+      NewsComponent, 
+      CalendarioComponent
+    ]
 })
+
 export class AppComponent {
   title = 'jornalESP';
 }
