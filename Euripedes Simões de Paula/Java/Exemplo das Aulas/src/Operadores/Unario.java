28ac9e8720a1fe775938e3e07c20cb4ddcc18918
Lucas Amaro <lucash.96@hotmail.com>
2024-06-03 00:36:46 -0300

    Terminando conteúdo sobre Operadores Java

--- Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Operadores/Unario.java
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Operadores/Unario.java
@@ -3,11 +3,11 @@ package Operadores;
 public class Unario {
 
 	public static void main(String[] args) {
-		
+			
 		int a = 10;
 		int b = 10;
 		
-/*		
+/*	
 
 // Primeiro Exemplo
 
@@ -20,9 +20,38 @@ public class Unario {
 		///Decrementando um valor antes da impressão
 		System.out.println(--a);
 */		
+
+/*	
+// Segundo Exemplo 
 		
-		System.out.println(a++ + ++a);
-		System.out.println(b++ + b++);
+		//Analisando a++ + ++a
+        // a++ retorna 10, depois a é incrementado para 11
+        // ++a incrementa a para 12, depois retorna 12
+        // 10 + 12 = 22
+		System.out.println(a++ + ++a);	// Saída: 22
+		// Analisando b++ + b++
+        // b++ retorna 10, depois b é incrementado para 11
+        // b++ retorna 11, depois b é incrementado para 12
+        // 10 + 11 = 21
+		System.out.println(b++ + b++);	// Saída: 21
+*/		
+		
+/*	*/
+// Terceiro Exemplo
+		int c = 10;
+		int d = -10;
+		boolean e = true;
+		boolean f = false;
+		
+		// Invertendo o sinal e subtraindo com 1
+		System.out.println(~c);
+		// Inverte o sinal e subtrai com 1
+		System.out.println(~d);
+		// Falso - oposto do valor booleano
+		System.out.println(!e);
+		// Verdadeiro - oposto do valor booleano
+		System.out.println(!f);
+		
 	}
 	
 }
