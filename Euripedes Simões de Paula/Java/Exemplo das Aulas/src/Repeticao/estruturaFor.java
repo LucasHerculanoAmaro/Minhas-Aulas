80d631dec134b9b979981a43014bc5406e21b902
Lucas Amaro <lucash.96@hotmail.com>
2024-06-07 23:12:42 -0300

    Trabalhando com aula: Laço de Repetição Java

---
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Repeticao/estruturaFor.java
@@ -0,0 +1,19 @@
+package Repeticao;
+
+public class estruturaFor {
+	
+	public static void main(String[] args) {
+		
+		// Conatndo com Incremento
+		for(int a = 0; a <= 10; ++a) { 
+			System.out.println("A variável A somou: " + a);
+		}
+		
+		// Comntando com Decremento
+		for(int b = 10; b >= 0; --b) {
+			System.out.println("A variável B subtraiu : " + b);
+		}
+		
+	}
+	
+}
