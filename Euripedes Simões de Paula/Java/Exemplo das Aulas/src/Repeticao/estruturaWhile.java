25d6d899dfa39f80de56bda4a30ce695b04063a4
Lucas Amaro <lucash.96@hotmail.com>
2024-06-12 22:49:28 -0300

    Trabalhando com Laço de Repetição

--- Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Repeticao/estruturaWhile.java
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Repeticao/estruturaWhile.java
@@ -1,5 +1,30 @@
 package Repeticao;
 
+import java.util.Scanner;
+
 public class estruturaWhile {
 
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int numero = -1;
+		while (numero != 10) {
+			
+			System.out.println("Digite um número: ");
+			numero = leia.nextInt();
+			
+			leia.close();
+			
+			if (numero == 10) {
+				System.out.println("\nVocê Acertou!!!");
+			}			
+			else {
+				System.out.println("Você errou...");
+			}
+			
+		}
+		
+	}
+	
 }
