0a95f65b703b49b251790c67feef85d8de10d77b
Lucas Amaro <lucash.96@hotmail.com>
2024-06-21 18:11:12 -0300

    Adicionando mais dois exemplos a aula de matrizes Java

--- Euripedes Simões de Paula/Java/Exemplo das Aulas/src/MatrizesVetores/Matrizes.java
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/MatrizesVetores/Matrizes.java
@@ -1,11 +1,13 @@
 package MatrizesVetores;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Matrizes {
 	
 	public static void main(String[] args) {
-		
+	
+/*
 		// Criando os Vetores
 		int[] vetorA = {1, 2, 3};
 		int[] vetorB = {4, 5, 6};
@@ -34,20 +36,74 @@ public class Matrizes {
 		int[][] matrizC = new int[2][3];
 		
 		// Soma das matrizes
-		for (int i = 0; i < matrizA.length; i++) {
-			for (int j = 0; j < matrizA[i].length; j++) {
-				matrizC [i][j] = matrizA[i][j] + matrizB [i][j];
+		for (int linha = 0; linha < matrizA.length; linha++) {
+			for (int coluna = 0; coluna < matrizA[linha].length; coluna++) {
+				matrizC [linha][coluna] = 
+						matrizA[linha][coluna] + matrizB [linha][coluna];
 			}
 		}
 		
 		// Imprime a soma das matrizes
 		System.out.println("Soma das matrizes: ");
-		for (int i = 0; i < matrizC.length; i++ ) {
-			for (int j = 0; j < matrizC[i].length; j++) {
-				System.out.println(matrizC[i][j] + " ");
+		for (int linha = 0; linha < matrizC.length; linha++ ) {
+			for (int coluna = 0; coluna < matrizC[linha].length; coluna++) {
+				System.out.println("Posição ["+ linha + "]["+  coluna + "] = " + matrizC[linha][coluna]);
 			}
 		}
-	
+*/
+		
+/*		Segundo Exemplo
+		int[][] matrizA = new int[3][3];
+		Scanner leia = new Scanner(System.in);
+		
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++ ) {
+				System.out.println(
+						"Insira o valor para a posição [" + linha + "][" + coluna + "]: ");
+				matrizA[linha][coluna] = leia.nextInt();
+			}
+		}
+		
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				System.out.println(
+						"O valor na posição [" + linha + "][" + coluna + "] é: "
+						+ matrizA[linha][coluna] );
+			}
+		}
+*/		
+		
+/*		Terceiro Exemplo */
+		int soma = 0;
+		int vetor[] = new int[4];
+		int[][] matrizA = new int[5][4];
+		
+		Scanner leia = new Scanner(System.in);
+		
+		// Adicionando os dados na matriz
+		for (int linha = 0; linha < 5; linha++) {
+			for (int coluna = 0; coluna < 4; coluna++) {
+				System.out.println("Insira um valor para a posição ["
+						+ linha + "][" + coluna + "]");
+				matrizA[linha][coluna] = leia.nextInt();
+			}
+		}
+		
+		for (int coluna = 0; coluna < 4; coluna++) {
+			for (int linha = 0; linha < 5; linha++) {
+				soma += matrizA[linha][coluna];
+			}
+			
+			vetor[coluna] = soma;
+			
+			soma = 0;
+		}
+		
+		for (int coluna = 0; coluna < 4; coluna++) {
+			System.out.println(
+					"Somando os elementos " + (coluna + 1) + " é " + vetor[coluna]);
+		}
+		
 		
 	}
 	
