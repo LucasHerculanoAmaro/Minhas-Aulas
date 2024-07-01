86255d96047cb79aab9e0b5350284eeb6137d3fc
Lucas Amaro <lucash.96@hotmail.com>
2024-06-30 23:59:28 -0300

    Trabalhando com hierarquia em Collections

---
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Collections/Collections.java
@@ -0,0 +1,25 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Collections {
+
+	public static void main(String[] args) {
+		
+/* ArrayList 
+ * 
+ * Abaixo implementamos */
+		
+		// Criando uma lista
+		List<String> list = new ArrayList<>();
+		// Adicionando valores a lista
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		// Pegando valores da lista
+		System.out.println(list.get(1));
+	}
+	
+}
