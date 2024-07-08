cc26ccc12074687254adf60a16f2cbea28dcdd48
Lucas Amaro <lucash.96@hotmail.com>
2024-07-07 22:45:40 -0300

    Terminando aula de Lambda

--- Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Lambda/ExpressaoLambda.java
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Lambda/ExpressaoLambda.java
@@ -1,8 +1,8 @@
 package Lambda;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExpressaoLambda {
 
@@ -51,7 +51,7 @@ public class ExpressaoLambda {
 * 		Nos exemplos a seguir será ulilizado as Interfaces Funcionais,
  		onde apenas um método abstrato será criado.
  
-* 		As anotações não são obrigatórias.
+* 		OBS:. As anotações não são obrigatórias.
  		
 */		
 		
@@ -93,6 +93,40 @@ public class ExpressaoLambda {
 //		Utilizando forEach com Lambda para imprimir
 		nomes.forEach(nome -> System.out.println(nome));
 		
+
+		System.out.println("\nCriando coleção stream e selelcionando os nomes com Lambda");
+	
+//		Vamos utilizar a lista á criada na linha 91...
+		
+		
+/*		CURIOSIDADE
+	
+*		A STREAM API oferece a possibilidade de trablalhar com conjunto 
+		de elementos de maneira mais simples. A proposta é diminuir a 
+		forma de implementar controle de fluxo ao lidar com collections.
+		
+*		Podemos trabalhar com os elementos de um objeto, realizando ações 
+		de filtragem, mapeamento, transformação, etc.	
+		
+*		Uma STREAM COLLECT() realiza uma operação de redução mutável nos 
+		elementos do fluxo. Isso significa que os elementos que são 
+		processados são acumulados em um objeto que podem sofrer alterações.
+		
+*		No exemplo abaixo, usamos junto como parâmetro do método 'collect()'
+		o método 'toList()', que acumula os elementos em uma List.	
+ */
+		
+		
+//		Criando uma lista e atribuindo stream ao objeto
+		List<String> filtroNomes = nomes.stream()
+				
+//										Criando filtro e adicionando expressão Lambda
+										.filter(nome -> nome.startsWith("P"))// || nome.startsWith("L"))
+//										Utilizando o método 'collect()' e 'toList()' como parâmetro
+										.collect(Collectors.toList());
+//		Selecionando o elemento da lista, ao qual passou pelo filtro
+		System.out.println(filtroNomes);
+		
 	}
 	
 }
@@ -105,7 +139,13 @@ public class ExpressaoLambda {
 * 		Java Virtual Thread: Conceitos e quando (ou não) usar
  		https://medium.com/@boschtechbr/java-virtual-thread-conceito-e-quando-ou-n%C3%A3o-usar-7c56238e951d#:~:text=Antes%20de%20mais%20nada%2C%20o,sistema%20operacional%E2%80%9D%20%5B1%5D.
  
- 
- 
- * 
- * */
+* 		Java Stream API: manipulando coleções de forma eficiente
+ 		https://www.devmedia.com.br/java-streams-api-manipulando-colecoes-de-forma-eficiente/37630
+ 		
+* 		Exemplos do método Java Stream Collect()
+ 		https://www.digitalocean.com/community/tutorials/java-stream-collect-method-examples
+
+*		Java 8 - Reduções e Parallel Streams
+		https://frozendo.medium.com/java-8-redu%C3%A7%C3%B5es-e-parallel-streams-a4a519084d55
+
+*/
