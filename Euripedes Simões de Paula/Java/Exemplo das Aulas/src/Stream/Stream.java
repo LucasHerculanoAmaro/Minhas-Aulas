69fc9ca1af3a4249700ae405443c9c128d31016c
Lucas Amaro <lucash.96@hotmail.com>
2024-07-08 16:50:02 -0300

    Iniciando aula de Stream Java

---
+++ Euripedes Simões de Paula/Java/Exemplo das Aulas/src/Stream/Stream.java
@@ -0,0 +1,53 @@
+package Stream;
+
+public class Stream {
+
+	public static void main(String[] args) {
+		
+/*	INTRODUÇÃO 
+ 
+ * 	Esta API foi introduzida no Java 8, e faz parte do pacote 
+	'java.util.stream'.
+	
+ *	Quase todas as aplicações Java criam e processam coleções,
+ 	onde agrupam e processam dados. Esses dados podem ser
+ 	consultados e alterados de maneira similar ao que já 
+ 	estudamos em SQL.
+	
+ *	A STREAM é um recurso muito utilizado, que oferece eficiência 
+ 	no processamento de dados (coleções) de forma declarativa, 
+ 	tornando mais simples, proporcionando um código limpo, com 
+ 	fácil manutenção e evitar erros.
+	
+ *	Ao processar os dados, é possível criar combinações nas 
+ 	operações, onde são incorporados o Paradigma Funcional e 
+ 	a Expressão Lambda.
+	
+	
+	PROGRAMAÇÃO COM STREAM
+	
+ *		
+	
+
+*/
+		
+		
+	}
+	
+}
+
+/*	REFERÊNCIAS
+
+ * 	Stream
+	https://github.com/Leon4rdoalves/CookBook-Java/blob/main/17.md
+	
+ * 	Processamento de dados com strea do Java SE 8 - Perte 1
+	https://www.oracle.com/br/technical-resources/articles/java/processing-streams-java-se-8.html
+
+ * 
+
+ *  
+
+ *
+  
+*/
