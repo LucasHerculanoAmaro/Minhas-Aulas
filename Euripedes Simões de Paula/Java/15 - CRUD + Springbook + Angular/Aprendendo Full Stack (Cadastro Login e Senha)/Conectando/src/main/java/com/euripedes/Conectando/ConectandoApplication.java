1261f2dfd057b0c6e687dce83887a5a7f097c70b
Lucas Amaro <lucash.96@hotmail.com>
2024-08-05 12:20:05 -0300

    Estudando sobre Spring Security

---
+++ Euripedes Simões de Paula/Java/15 - CRUD + Springbook + Angular/Aprendendo Full Stack (Cadastro Login e Senha)/Conectando/src/main/java/com/euripedes/Conectando/ConectandoApplication.java
@@ -0,0 +1,13 @@
+package com.euripedes.Conectando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectandoApplication.class, args);
+	}
+
+}
