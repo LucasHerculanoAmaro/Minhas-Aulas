5068c836f93a8324480c593c4cb09ec5b839884d
Lucas Amaro <lucash.96@hotmail.com>
2024-10-18 09:11:16 -0300

    Primeiras implementações do método READ

---
+++ Euripedes Simões de Paula/Java/15 - CRUD + Springboot + Angular/Alunos-font-end/src/app/alunos-lista/alunos-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './alunos-lista.component.html',
+  styleUrl: './alunos-lista.component.css'
+})
+export class AlunosListaComponent {
+
+}
---
+++ Euripedes Simões de Paula/Java/15 - CRUD + Springboot + Angular/Alunos-font-end/src/app/alunos.ts
@@ -0,0 +1,12 @@
+/*  Criamos uma classe de domínio Typescript, e vamos definir algumas propriedades que serão 
+    chamadas futuramente.     */
+export class Alunos {
+
+    // Abaixo vamos criar os mesmos atributos existente em nossa aplicação banck-end.
+    id!: number;
+    nome!: String;
+    sobrenome!: String;
+    email!: String;
+}
+
+/*  Terminando esta implementação, vamos ao terminal e utilizar o comando "ng g c alunos-lista". */
