dca34373c8540bb04e1a7f6df8ab29b074495bf3
unknown <lucash.96@hotmail.com>
2024-11-26 12:33:05 -0300

    Trabalhando com cores para valores negativo e positivos no Razão

--- Euripedes Simões de Paula/Java/16 - Trabalho de Conclusão de Curso/Aprendendo Full Stack Angular/Conectando/src/app/razao/razao.component.ts
+++ Euripedes Simões de Paula/Java/16 - Trabalho de Conclusão de Curso/Aprendendo Full Stack Angular/Conectando/src/app/razao/razao.component.ts
@@ -1,29 +1,24 @@
-import { Component } from '@angular/core';
-import { Razao } from '../model/Razao';
-import { RazaoService } from '../services/razao.service';
+import { Component, OnInit } from '@angular/core';
 import { Diario } from '../model/Diario';
 import { DiarioService } from '../services/diario.service';
-import { style } from '@angular/animations';
-import { HtmlTagDefinition } from '@angular/compiler';
-import { verify } from 'crypto';
 
 @Component({
   selector: 'app-razao',
   templateUrl: './razao.component.html',
   styleUrl: './razao.component.css'
 })
-export class RazaoComponent {
+export class RazaoComponent implements OnInit {
 
-  razao : Diario[] = [];
+  razao: Diario[] = [];
 
-  constructor( private diarioService: DiarioService ) {}
+  constructor(private diarioService: DiarioService) { }
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.getLancamentos();
   }
 
   // Soma do Crédito
-  get totalCredito() : number {
+  get totalCredito(): number {
 
     return this.razao
       .filter((item) => item.transacao === "CRÉDITO")
@@ -31,14 +26,14 @@ export class RazaoComponent {
   }
 
   // Soma do Débito
-  get totalDebito() : number {
+  get totalDebito(): number {
     return this.razao
       .filter((item) => item.transacao === "DÉBITO")
       .reduce((total, item) => total + item.valor, 0);
   }
 
   // Soma Total
-  get saldoGeral() : number {
+  get saldoGeral(): number {
     return this.totalCredito - this.totalDebito;
   }
 
@@ -47,6 +42,26 @@ export class RazaoComponent {
     this.diarioService.getLancamentos().subscribe(data => {
       this.razao = data;
     })
+  }
+
+  // Método para Débito
+  isDebito(transacao: string): boolean {
+    return transacao.toLowerCase() === "débito";
+  }
+
+  // Método para Crédito
+  isCredito(transacao: string): boolean {
+    return transacao.toLowerCase() === "crédito";
+  }
+
+  // Método para Total
+  positiveNegative(status: boolean) {
+    if ( this.totalCredito > this.totalDebito ) {
+      status = true;
+    } else {
+      status = false;
+    }
+     
   }
 
 }
